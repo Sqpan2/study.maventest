43bcf59ffc8ae796c04dddbb38e85f9980d416d8
Sqpan2 <951514058@qq.com>
2018-02-21 22:38:18 +0800

    study.maventest
    使用github搭建maven私库，这是个jar工程，用于模拟上传jar

---
+++ src/main/java/com/panshiqi/study/maventest/TestUtil.java
@@ -0,0 +1,18 @@
+/**
+ * @company   : com.sqpan2.study
+ * @poroject   : sqpan2 study
+ * @copyright : copyright (c) 2017
+ * 
+ * @description: TODO
+ * @author   : Sqpan2
+ * @date     : 2018年2月7日 下午8:59:39
+ * @version  : v1.0
+ */
+package com.panshiqi.study.maventest;
+
+public class TestUtil {
+
+	public static void printHello() {
+		System.out.println("========== HELLO version 0.0.1-SNAPSHOT =========");
+	}
+}
